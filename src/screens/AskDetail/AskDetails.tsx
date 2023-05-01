7677cc4d46704a8f5a38500ae8d7763fe27c13f7
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-05-01 14:48:42 +0100

    Ask Detail screen template

---
+++ src/screens/AskDetail/AskDetails.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+
+
+export default function AskDetails(){
+
+
+    return (
+        <View>
+            <View>
+                <Text> Ask Detail</Text>
+            </View>
+        </View>
+    )
+}
